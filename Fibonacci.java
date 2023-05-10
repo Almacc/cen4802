5df3a84e59a23e33dfce145ba2ca8984c22813a5
Alessandro Maccarrone <66288576+Almacc@users.noreply.github.com>
2023-05-10 09:57:38 -0400

    Update Fibonacci.java

--- Fibonacci.java
+++ Fibonacci.java
@@ -1,19 +1,35 @@
+/**
+ * The Fibonacci class calculates the nth term in the Fibonacci sequence using recursion.
+ * The Fibonacci sequence is a series of numbers in which each number is the sum of the two preceding ones.
+ * This class provides a recursive method to calculate the nth term in the Fibonacci sequence.
+ *
+ * @author Alessandro Maccarrone
+ */
 public class Fibonacci {
-    public static void main(String[] args) {
-        int inputs = 10;
 
-            int recursiveResult = recursive(inputs);
-
-            System.out.println("The " + inputs + " term of the Fibonacci sequence is: " + recursiveResult);
-
-    }
-
-
+    /**
+     * Calculates the nth term in the Fibonacci sequence using recursion.
+     *
+     * @param n the position of the term to calculate
+     * @return the nth term in the Fibonacci sequence
+     */
     public static int recursive(int n) {
         if (n <= 1) {
             return n;
         } else {
             return recursive(n - 1) + recursive(n - 2);
         }
+    }
+
+    /**
+     * Main method that calls the recursive method to calculate the nth term in the Fibonacci sequence.
+     * Outputs the result with descriptive text.
+     *
+     * @param args command-line arguments (not used)
+     */
+    public static void main(String[] args) {
+        int n = 10;
+        int result = recursive(n);
+        System.out.println("The " + n + "th term of the Fibonacci sequence is: " + result);
     }
 }
